29f89183bc90323f4e197aabbc9d84be5c161acc
Michael Swart <mswart@princeton.edu>
2017-10-23 14:46:28 -0400

    file submission start

--- public/app.js
+++ public/app.js
@@ -1,3 +1,34 @@
+function fileSubmit() {
+  var x = document.getElementById("file")
+  var txt = "";
+  var file_reader = new FileReader;
+    if ('files' in x) {
+        if (x.files.length == 1) {
+            for (var i = 0; i < x.files.length; i++) {
+                var file = x.files[i];
+                if ('name' in file) {
+                    txt += "name: " + file.name + "\t";
+                }
+                if ('size' in file) {
+                    txt += "size: " + file.size + " bytes \n";
+                }
+                txt += "\tThe file is of type " + file.type;
+            }
+        }
+        else if (x.files.length > 1) {
+            alert("Please select only one file to encrypt");
+        } 
+    } 
+    else {
+        if (x.value == "") {
+            alert("Select a file to encrypt");
+        } else {
+            alert("File submission not supported");
+        }
+    }
+    console.log(txt)
+}
+
 document.addEventListener("DOMContentLoaded", function(event) {
   document.getElementById('signin-button').addEventListener('click', function(event) {
     event.preventDefault()
